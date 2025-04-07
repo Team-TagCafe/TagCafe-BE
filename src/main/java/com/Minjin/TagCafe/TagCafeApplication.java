c1d3fc88b5dd8b8b75cf10898b5aef753d81ef00
jinlee <dlwls6877@naver.com>
2025-04-07 15:15:20 +0900

    [fix]#18-환경변수 참조대신 .env에서 직접 읽도록 명시

--- src/main/java/com/Minjin/TagCafe/TagCafeApplication.java
+++ src/main/java/com/Minjin/TagCafe/TagCafeApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TagCafeApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		Dotenv dotenv = Dotenv.configure().directory("/home/ec2-user/app").ignoreIfMissing().load();
 
 		System.setProperty("KAKAO_CLIENT_ID", dotenv.get("KAKAO_CLIENT_ID"));
 		System.setProperty("KAKAO_REDIRECT_URI", dotenv.get("KAKAO_REDIRECT_URI"));
