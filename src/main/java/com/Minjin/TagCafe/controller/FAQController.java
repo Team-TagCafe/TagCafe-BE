d8821387251993ecfc1cecaecf813576aa23258e
김민지 <ghi512151@gmail.com>
2025-04-16 18:24:58 +0900

    [feat] #22 - faq, review에 어노테이션 추가

--- src/main/java/com/Minjin/TagCafe/controller/FAQController.java
+++ src/main/java/com/Minjin/TagCafe/controller/FAQController.java
@@ -7,6 +7,8 @@ import com.Minjin.TagCafe.entity.Feedback;
 import com.Minjin.TagCafe.entity.QA;
 import com.Minjin.TagCafe.repository.FeedbackRepository;
 import com.Minjin.TagCafe.repository.QARepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Tag(name = "FAQ", description = "자주 묻는 질문 및 사용자 피드백 관련 API")
 @RestController
 @RequestMapping("/faq")
 @CrossOrigin(origins = "http://tagcafe.site")
@@ -30,6 +33,7 @@ public class FAQController {
 
 
     //자주 묻는 질문 조회
+    @Operation(summary = "자주 묻는 질문 조회", description = "모든 QA 데이터를 조회합니다.")
     @GetMapping("/qa")
     public ResponseEntity<List<QA>> getQAs() {
         List<QA> qaList = qaRepository.findAll();
@@ -37,6 +41,7 @@ public class FAQController {
     }
 
     //사용자 의견 및 오류 제보 제출
+    @Operation(summary = "사용자 피드백 제출", description = "사용자의 의견이나 오류 제보를 저장합니다.")
     @PostMapping("/feedback")
     public ResponseEntity<Map<String, String>> submitFeedback(@RequestBody FeedbackRequest request) {
         Feedback feedback = new Feedback();
@@ -51,6 +56,7 @@ public class FAQController {
     }
 
     // ✅ 관리자용 - 사용자 의견 및 오류 목록 조회 API
+    @Operation(summary = "admin - 사용자 피드백 전체 조회", description = "관리자가 보는 사용자 피드백 목록입니다.")
     @GetMapping("/feedback")
     public ResponseEntity<List<FeedbackResponse>> getFeedbackList() {
         List<FeedbackResponse> feedbacks = feedbackRepository.findAll()
