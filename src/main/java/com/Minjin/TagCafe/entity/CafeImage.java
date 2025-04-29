c5303a0bcfe6285c5a5153c7bca334cf65545610
김민지 <ghi512151@gmail.com>
2025-04-30 01:45:13 +0900

    [delete] #27 - CafeImage 테이블에서 imageData 컬럼 삭제

--- src/main/java/com/Minjin/TagCafe/entity/CafeImage.java
+++ src/main/java/com/Minjin/TagCafe/entity/CafeImage.java
@@ -14,10 +14,6 @@ public class CafeImage {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Lob
-    @Column(name = "image_data", columnDefinition = "LONGBLOB")
-    private byte[] imageData; // 이미지 바이너리
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cafe_id")
     private Cafe cafe;
